3b38925cf9ee16c38b678549915b243a636341e5
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 13:32:49 +0200

    Adding code examples/financial-data_golang.go

---
+++ code examples/financial-data_golang.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	const Username = "YOUR_USERNAME"
+	const Password = "YOUR_PASSWORD"
+
+	payload := map[string]string{
+		"source": "universal",
+		"url": "https://www.syntaxdata.com/affinity?_gl=1*1ozt4xh*_up*mq..*_ga*mtuyntq3ntm5lje3mdi1ndixotc.*_ga_v0892cy751*mtcwmju0mje5ni4xljaumtcwmju0mje5ni4wljauma..",
+	}
+
+	jsonValue, _ := json.Marshal(payload)
+
+	client := &http.Client{}
+	request, _ := http.NewRequest("POST",
+		"https://realtime.oxylabs.io/v1/queries",
+		bytes.NewBuffer(jsonValue),
+	)
+
+	request.SetBasicAuth(Username, Password)
+	response, _ := client.Do(request)
+
+	responseText, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(responseText))
+}
