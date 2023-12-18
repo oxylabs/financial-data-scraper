ef33801bf7b5f5a122b54b9535013b21af962e28
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 13:32:50 +0200

    Adding code examples/financial-data_curl.sh

---
+++ code examples/financial-data_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://www.syntaxdata.com/affinity?_gl=1*1ozt4xh*_up*mq..*_ga*mtuyntq3ntm5lje3mdi1ndixotc.*_ga_v0892cy751*mtcwmju0mje5ni4xljaumtcwmju0mje5ni4wljauma.."}'
